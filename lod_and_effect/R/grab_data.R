57d0da83fcf98c0a1714ebaf9a59cc6fa7994b75
Karl Broman <kbroman@gmail.com>
2013-02-12 20:57:40 -0600

    lod_and_effect: Revise grab_data.R to grab phenotype, sex and genotypes
      - genotypes include imputed values; negative if original genotype
        was empty or a mismatch
    
      - X chromosome will be 1/2 values for both males and females
    
      - sex will be 0 = female and 1 = male

--- lod_and_effect/R/grab_data.R
+++ lod_and_effect/R/grab_data.R
@@ -14,7 +14,7 @@ f2g <- calc.genoprob(f2g, step=0.5, stepwidth="max", error.prob=0.002, map.funct
 f2g$pheno$insulin <- rowMeans(lipomics[,c(37,41)], na.rm=TRUE)
 
 # genome scan with sex as interactive covariate
-sex <- as.numeric(f2g$pheno$Sex)
+sex <- as.numeric(f2g$pheno$Sex)-1
 out <- scanone(f2g, phe="insulin", addcovar=sex, intcovar=sex, method="hk")
 
 f2g <- sim.geno(f2g, step=0, error.prob=0.002, map.function="c-f", n.draws=128)
@@ -45,14 +45,24 @@ for(i in seq(along=map)) {
 }
 markers <- lapply(mar, names)
 
+phevals <- f2g$pheno$insulin
+f2gi <- pull.geno(fill.geno(f2g, err=0.002, map.function="c-f"))
+g <- pull.geno(f2g)
+f2gi[is.na(g) | f2gi != g] <- -f2gi[is.na(g) | f2gi != g]
+f2gi <- as.list(as.data.frame(f2gi))
+
 # write data to JSON file
 library(RJSONIO)
 cat0 <- function(...) cat(..., sep="", file="../insulinlod.json")
 cat0a <- function(...) cat(..., sep="", file="../insulinlod.json", append=TRUE)
-cat0("{\n\"phenotype\" : \"", phenotype, "\",\n\n")
+cat0("{\n")
+cat0a("\"phenotype\" : \"", phenotype, "\",\n\n")
 cat0a("\"chr\" :\n", toJSON(chrnames(f2g)), ",\n\n")
 cat0a("\"lod\" :\n", toJSON(lapply(split(out, out[,1]), function(a) as.list(a[,2:3])), digits=8), ",\n\n")
 cat0a("\"markerindex\" :\n", toJSON(mar), ",\n\n")
 cat0a("\"markers\" :\n", toJSON(markers), ",\n\n")
-cat0a("\"effects\" :\n", toJSON(qtleffects, digits=8), "\n\n")
+cat0a("\"effects\" :\n", toJSON(qtleffects, digits=8), ",\n\n")
+cat0a("\"phevals\" :\n", toJSON(phevals, digits=8), ",\n\n")
+cat0a("\"sex\" :\n", toJSON(sex), ",\n\n")
+cat0a("\"geno\" :\n", toJSON(f2gi), "\n\n")
 cat0a("}\n")
